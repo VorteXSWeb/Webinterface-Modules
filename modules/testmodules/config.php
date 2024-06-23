72d4331c1d013a5582e1435f4b7500a1ed6639c4
VorteXS <tomm46072@gmail.com>
2024-06-23 18:27:09 +0200

    Update config.php

---
+++ modules/testmodules/config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array();
+$config['modulesname'] = "Test : Testing Modules";
+$config['modulesversion'] = "1.0.0";
+$config['modulesdesc'] = "Test Modules for Learning Creating";
+$config['modulesadress'] = "https://vortexs.de";
+$config['modulesupdate'] = "https://vortexs.de/update";
+?>
